65b9886cf078be4793a286ab91f8da52bf698db5
zJonchx <jonathaneliaschavez123@gmail.com>
2025-06-29 18:22:32 -0300

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ IMAGE_URLS = {
     "users": "https://cdn.discordapp.com/attachments/1379254610119753910/1388938977515540700/3e328f261afa2e6ab5379bdad3696d30.jpg",
     "info": "https://cdn.discordapp.com/attachments/1379254610119753910/1388938977515540700/3e328f261afa2e6ab5379bdad3696d30.jpg",
     "attack": "https://cdn.discordapp.com/attachments/1379254610119753910/1388939337000812584/killua-anime.gif",
-    "amp": "https://cdn.discordapp.com/attachments/1379254610119753910/1388939535282344137/221577.gif"
+    "amp": "https://cdn.discordapp.com/attachments/1379254610119753910/1388939535282344137/221577.gif",
     "free": "https://cdn.discordapp.com/attachments/1379254610119753910/1388991274367058092/4331-solo-leveling-god.png"
 }
 
